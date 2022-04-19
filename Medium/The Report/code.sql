f59bce99e7523f3f771c1515577b983dea52e211
Vuon Minh Nguyen <54706711+minhvuon@users.noreply.github.com>
2022-04-19 08:23:17 +0700

    Update code.sql

--- Medium/The Report/code.sql
+++ Medium/The Report/code.sql
@@ -1,5 +1,4 @@
 select if(g.grade > 7, s.name, null), g.grade, s.marks
 from students s, grades g
-where marks >= min_mark 
-    and marks <= max_mark
-order by g.grade desc, s.name asc, s.marks asc
+where marks between min_mark and max_mark
+order by g.grade desc, s.name asc, s.marks asc;
